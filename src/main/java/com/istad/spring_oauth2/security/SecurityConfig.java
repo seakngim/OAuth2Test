eb710c869945c943fe91cf4d94369bffaf49ca93
SEAKNGIM <seakngimsr37@gmail.com>
2024-09-02 14:26:37 +0700

    first commit

---
+++ src/main/java/com/istad/spring_oauth2/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.istad.spring_oauth2.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+
+public class SecurityConfig {
+    @Bean
+    @Order(1)
+    SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
+        http
+                // Redirect to the login page when not authenticated from the
+                // authorization endpoint
+                .exceptionHandling((exceptions) -> exceptions
+                        .defaultAuthenticationEntryPointFor(
+                                new LoginUrlAuthenticationEntryPoint("/login"),
+                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                        )
+                )
+                // Accept access tokens for User Info and/or Client Registration
+                .oauth2ResourceServer((resourceServer) -> resourceServer
+                        .jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+
+    @Bean // Bean of Web Security
+    @Order(2)
+    SecurityFilterChain WebSecurityFilterChain(HttpSecurity http) throws Exception {
+
+
+        return http.build();
+    }
+}
---
+++ src/test/java/com/istad/spring_oauth2/SpringOauth2ApplicationTests.java
@@ -0,0 +1,10 @@
+package com.istad.spring_oauth2;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringOauth2ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
